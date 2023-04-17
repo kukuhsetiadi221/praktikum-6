573b6633b8a4a8696f88a33298fc5a30f67a607a
kukuhsetiadi221 <kukuhsetiadi@students.amikom.ac.id>
2023-04-17 15:20:52 +0700

    Add project files.

---
+++ praktikum 6/Motor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikum_6
+{
+    public class Motor
+    {
+        private string merek;
+        private int tahun;
+        private int harga;
+        private string warna;
+
+
+        public string Merek
+        {
+            get { return merek; }
+            set { merek = value; }
+        }
+        public int Tahun
+        {
+            get { return tahun; }
+            set { tahun = value; }
+        }
+        public string Warna
+        {
+            get { return warna; }
+            set { warna = value; }
+        }
+        public int Harga
+        {
+            get { return harga; }
+            set { harga = value; }
+        }
+
+        public void InfoMotor()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(" Merek Motor : {0}", Merek);
+            Console.WriteLine(" Tahun Produksi : {0}", Tahun);
+            Console.WriteLine(" Warna Motor : {0}", Warna);
+            Console.WriteLine(" Harga Motor : {0} juta", Harga);
+        }
+    }
+}
---
+++ praktikum 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace praktikum_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motor1 = new Motor();
+            motor1.Merek=("Vario");
+            motor1.Tahun=(2022);
+            motor1.Warna=("Hitam");
+            motor1.Harga=(28);
+
+            Motor motor2 = new Motor();
+            motor2.Merek=("Nmax");
+            motor2.Tahun=(2020);
+            motor2.Warna=("Hitam");
+            motor2.Harga=(34);  
+
+            motor1.InfoMotor();
+            motor2.InfoMotor();
+            Console.ReadLine();
+
+        }
+    }
+}
